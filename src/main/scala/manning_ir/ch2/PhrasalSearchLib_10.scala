dc559475d164599834c6692a0fb45268d1f304e2
GitOutATown <aqumegeemail@gmail.com>
2015-04-01 19:08:45 -0700

    backup

--- src/main/scala/manning_ir/ch2/PhrasalSearchLib_10.scala
+++ src/main/scala/manning_ir/ch2/PhrasalSearchLib_10.scala
@@ -111,10 +111,8 @@ object PhrasalSearchLib_10 {
 	:List[scala.collection.immutable.Map[Long, Incidences]] = {
 		// tokenize and lowercase query
 		val qTerms = preprocess(query)
-		
 		// Disjunction of query terms, initial step
 		val termsRetrieved = qTerms.map(dictionary.get(_)).flatten
-		 
 		val rankedIncidences = termsRetrieved.map( termIn => { // iterate over each Term
 			val termIncidences = termIn.allIncidences.values.map(docLocations => { // iterate through all document locations of this term
 				val termWordCount = docLocations.termCount.toDouble // total count of term in this document
@@ -130,8 +128,51 @@ object PhrasalSearchLib_10 {
 			termIncidences.toMap
 		}) // end termsRetrieved.map
 		rankedIncidences.toList
-		
-	} // end phrasalQuery
+	} // end freqRank
+	
+	// Dependancy on preserved order of query in results
+	// TODO: What about biword ranking?
+	// What about assembling tuples of as much of the phrase as is found?
+	// QueryTermResults = List[scala.collection.immutable.Map[Long, Incidences]]
+	//                  = ListOfAllTerms[OneTerm'sMapOfAllDocs[docId, OneDocIncidences]]
+	def biWord(queryTermResults: QueryTermResults): List[List[BiWord]] = {
+		// Recursive
+		def inter(terms: QueryTermResults, accum: List[List[BiWord]]): List[List[BiWord]] = {
+			terms match {
+				case leftTerm :: Nil => accum
+				case leftTerm :: xs => { // leftTerm is a Map of Instances per docId for this term
+					val biwords = for{
+			  			leftDocIncidences <- leftTerm.values // iterate each individual document's Incidences for this term
+						leftTermIndexInDoc <- leftDocIncidences.locs.indexes // iterate each location of the left term in this doc
+	   					leftTermStr = leftDocIncidences.locs.term  //leftDocQueryResult.docIncidences.term
+	   					docId = leftDocIncidences.locs.docId // the id of the doc being queried
+	   					// Now to see if the next term has this document, and if so,
+	   					// does the document have a location value of leftIndex + 1
+	   					rightTermLocationIndex:Int = xs.head.get(docId) match { // right term locations in this document
+	   						case Some(incds) => { // Yes, this document also contains the next term in the phrase
+	   							// So does that term exist at leftIndex + 1 ?
+	   							incds.locs.indexes.toList.indexOf(leftTermIndexInDoc + 1)
+	   						}
+	   						case None => -1
+	   					}  						
+	  					if rightTermLocationIndex > -1 // if true, we found a sequential pair
+			  		} yield {
+			  			// TODO: This series of dots is ugly
+			  			val rightTermIndexInDoc = xs.head.get(docId).get.locs.indexes.toList(rightTermLocationIndex)
+			  			val rightTermStr = xs.head.get(docId).get.locs.term
+			  			BiWord(
+			  				docId,
+			  				leftTermIndexInDoc, rightTermIndexInDoc,
+			  				leftTermStr, rightTermStr
+			  			)
+		  			}
+			  		inter(xs, accum ++ List(biwords.toList)) // recurse next term
+				} // end case x :: xs
+				
+			} // end terms match
+		} // end inter
+		inter(queryTermResults, List.empty[List[BiWord]])
+	} // end biWord
 }
 
 
--- src/main/scala/manning_ir/ch2/PhrasalSearch_lab_12.scala
+++ src/main/scala/manning_ir/ch2/PhrasalSearch_lab_12.scala
@@ -15,7 +15,7 @@ import scala.math.log10
  * Using Term Frequency for ranking. Not using IDF because collection is too small.
  * Some open questions:
  * * Whether to use a sliding window strategy (on the phrase query)
- * * Basis for phrase retreival ranking
+ * * Basis for phrase retrieval ranking
  * * Whether to use a proximity parameter on biword pairs (affects ranking)
  * * How to account for repeat terms in query
  */
@@ -46,7 +46,7 @@ object PhrasalSearch_lab_12 extends App {
  
   	// ------ Logging indexing results ------ //
 	
-	dictionary.keys.size
+	println("Dictionary terms size: " + dictionary.keys.size)
 	
 	docList foreach(doc => {
 		println("------------------")
@@ -60,10 +60,10 @@ object PhrasalSearch_lab_12 extends App {
 	val queryFrequency = freqRank(5)_ // set rank rounding
 	//val queryResults = queryFrequency("Look, where they come")
 	//val queryResults = queryFrequency("Take but good note")
-	//val queryResults = queryFrequency	("pillar of the world")
-	val queryResults = queryFrequency	("our dungy earth alike")
-	//val queryResults = queryFrequency	("earth alike")
-	//val queryResults = queryFrequency	("And, in conclusion, Elvis")
+	//val queryResults = queryFrequency("pillar of the world")
+	val queryResults = queryFrequency("our dungy earth alike")
+	//val queryResults = queryFrequency("earth alike")
+	//val queryResults = queryFrequency("And, in conclusion, Elvis")
 	
 	queryResults foreach(
 		queryResults => {
@@ -73,55 +73,9 @@ object PhrasalSearch_lab_12 extends App {
 			})
 		}
 	)
-  // Dependancy on preserved order of query in results
-  // TODO: What about biword ranking?
-  // What about assembling tuples of as much of the phrase as is found?
-  // Need to couple terms with locations
-  // QueryTermResults = List[scala.collection.immutable.Map[Long, Incidences]]
-  //                  = ListOfAllTerms[OneTerm'sMapOfAllDocs[docId, OneDocIncidences]]
-  def biWord(queryTermResults: QueryTermResults): List[List[BiWord]] = {
-		// Recursive
-		def inter(terms: QueryTermResults, accum: List[List[BiWord]]): List[List[BiWord]] = {
-			terms match {
-				case leftTerm :: Nil => accum
-				case leftTerm :: xs => { // leftTerm is a Map of Instances per docId for this term
-					val biwords = for{
-			  			leftDocIncidences <- leftTerm.values // iterate each individual document's Incidences for this term
-						leftTermIndexInDoc <- leftDocIncidences.locs.indexes // iterate each location of the left term in this doc
-	   					leftTermStr = leftDocIncidences.locs.term  //leftDocQueryResult.docIncidences.term
-	   					docId = leftDocIncidences.locs.docId // the id of the doc being queried
-	   					// Now to see if the next term has this document, and if so,
-	   					// does the document have a location value of leftIndex + 1
-	   					rightTermLocationIndex:Int = xs.head.get(docId) match { // right term locations in this document
-	   						case Some(incds) => { // Yes, this document also contains the next term in the phrase
-	   							// So does that term exist at leftIndex + 1 ?
-	   							incds.locs.indexes.toList.indexOf(leftTermIndexInDoc + 1)
-	   						}
-	   						case None => -1
-	   					}  						
-	  					if rightTermLocationIndex > -1 // if true, we found a sequential pair
-			  		} yield {
-			  			// TODO: This series of dots is ugly
-			  			val rightTermIndexInDoc = xs.head.get(docId).get.locs.indexes.toList(rightTermLocationIndex)
-			  			val rightTermStr = xs.head.get(docId).get.locs.term
-			  			BiWord(
-			  				docId,
-			  				leftTermIndexInDoc, rightTermIndexInDoc,
-			  				leftTermStr, rightTermStr
-			  			)
-		  			}
-			  		inter(xs, accum ++ List(biwords.toList)) // recurse next term
-				} // end case x :: xs
-				
-			} // end terms match
-		} // end inter
-		
-		inter(queryTermResults, List.empty[List[BiWord]])
-		
-  } // end biWord
   
-  val biWordResults = biWord(queryResults)
-  biWordResults foreach println
+	val biWordResults = biWord(queryResults)
+	biWordResults foreach println
 }
 /*
 
